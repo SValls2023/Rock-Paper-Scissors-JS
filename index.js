e8eb82a6493ae0e931e1ee68c26edebbe0b5dd78
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-11-18 20:08:37 -0800

    Feature: Players can now choose rock, paper, or scissors using buttons. Removed round sytem currently

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 // Main function where Rock Paper Scissors game takes place
 function game() {
-    console.log("Weclome to Rock Paper Scissors!");
+    // Deletion: console.log("Weclome to Rock Paper Scissors!");
+    let rps_buttons = document.querySelectorAll('.choice');
     let rounds = 1;
     let playerPoints = 0;
     let cpuPoints = 0;
-    while (rounds < 6 && (playerPoints < 3 && cpuPoints < 3)) {
-        playerChoice = prompt("ROUND " + (rounds) + "! Rock, paper, or scissors?");
-        if (playerChoice === null) continue;
-        else { 
-            playerChoice = playerChoice.toLowerCase();
-            if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-                let computerChoice = getComputerChoice();
-                let roundWinner = playRound(playerChoice, computerChoice);
-                if (roundWinner == 1) {
-                    alert("You lost, " + playerChoice + " loses against " + computerChoice + "!\n"
+
+    for(let i = 0; i < rps_buttons.length; i++) {
+        rps_buttons[i].addEventListener('click', () => {
+            const playerChoice = rps_buttons[i].value;
+            const computerChoice = getComputerChoice();
+            const roundWinner = playRound(playerChoice, computerChoice);
+            if (roundWinner === 1) {
+                console.log("You lost, " + playerChoice + " loses against " + computerChoice + "!\n"
                             + "Player: " + playerPoints + " CPU: " + (cpuPoints + 1));
-                    cpuPoints++;
-                    rounds++;
-                }
-                else if (roundWinner == 2) {
-                    alert("You win, " + playerChoice + " beats " + computerChoice + "!\n"
-                            + "Player: " + (playerPoints + 1) + " CPU: " + cpuPoints);
-                    playerPoints++;
-                    rounds++;
-                }
-                else {
-                    alert("It's a tie! This round will not count! Please select your weapon again.\n"
-                            + "Player: " + playerPoints + " CPU: " + cpuPoints);
-                }
-
-            } else {
-                alert("Could not understand option, please select your weapon again.");
+                cpuPoints++;
+                rounds++;
             }
-        }
+            else if (roundWinner === 2) {
+                console.log("You win, " + playerChoice + " beats " + computerChoice + "!\n"
+                            + "Player: " + (playerPoints + 1) + " CPU: " + cpuPoints);
+                playerPoints++;
+                rounds++;
+            }
+            else {
+                console.log("It's a tie! This round will not count! Please select your weapon again.\n"
+                            + "Player: " + playerPoints + " CPU: " + cpuPoints);
+            }
+        });
     }
 
-    if (playerPoints > cpuPoints) {
-        alert("You win the competition! Player: " + playerPoints + " CPU: " + cpuPoints);
-    }
-    else {
-        alert("The CPU won the competition! Player: " + playerPoints + " CPU: " + cpuPoints);
-    }
+    // if (playerPoints === 5) {
+    //     alert("You win the competition! Player: " + playerPoints + " CPU: " + cpuPoints);
+    // }
+    // else {
+    //     alert("The CPU won the competition! Player: " + playerPoints + " CPU: " + cpuPoints);
+    // }
 }
 
 // Decides if the player wins, loses, or ties with the computer for the round!
